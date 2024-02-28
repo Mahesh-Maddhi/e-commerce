bfc19eb343da1d044e04590b04afb1cb0c19b666
Mahesh Maddhi <maddimahesh111@gmail.com>
2024-02-28 12:22:12 +0530

    navbar fixed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# media files 
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -133,3 +137,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     
 ]
+
+# To add Statics directory
